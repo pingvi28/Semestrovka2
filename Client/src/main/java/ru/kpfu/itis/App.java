67699f4031ae58feb821bd181232c8ff82fe6c8c
Insaf Fayzrakhmanov <insafanas@mail.ru>
2021-12-16 00:29:02 +0300

    Template

---
+++ Client/src/main/java/ru/kpfu/itis/App.java
@@ -0,0 +1,11 @@
+package ru.kpfu.itis;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
