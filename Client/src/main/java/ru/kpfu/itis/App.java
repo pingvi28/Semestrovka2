1d35d334bccfb3ec9778fbb58acc317e00d13a3d
Di <kashapova.dilyara2016@yandex.ru>
2021-12-26 19:34:54 +0300

    for Liza =)

--- Client/src/main/java/ru/kpfu/itis/App.java
+++ Client/src/main/java/ru/kpfu/itis/App.java
@@ -17,17 +17,6 @@ public class App extends Application {
     }
 
     public void start(Stage primaryStage) throws Exception {
-        scene = new Scene(loadFXML("/ru.kpfu.itis/game.fxml"));
-        this.primaryStage = primaryStage;
-        this.primaryStage.setMinWidth(1000);
-        this.primaryStage.setMinHeight(680);
-        this.primaryStage.setScene(scene);
-        this.primaryStage.setResizable(false);
-        this.primaryStage.show();
-    }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml));
-        return fxmlLoader.load();
     }
 }
